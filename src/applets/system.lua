a9556ef6fe016625730b8114a983dcd4995f5758
Lina Plus Plus <TenmaTengo@protonmail.com>
2026-03-03 22:43:19 -0500

    add debug flags

--- src/applets/system.lua
+++ src/applets/system.lua
@@ -1,8 +1,23 @@
+
+if kernal.FLAGS.trace then
+    kernal.globals_logging(true);
+end
+
+if kernal.FLAGS["full-trace"] then
+    kernal.globals_logging(true);
+    kernal.verbose_thread_logging(true);
+end
+
 if #kernal.LOAD_APPLETS > 0 then
     for k,v in ipairs(kernal.LOAD_APPLETS) do
         import(v);
     end
 else
+
+    -- this is on because people will become 
+    -- confused at the lack of feedback
+    kernal.globals_logging(true);
+    
     -- TODO run the REPL under a new group
     -- TODO handle thr error returns
     repl();
